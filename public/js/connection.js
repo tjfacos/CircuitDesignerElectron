de2c5aa79865dca5dc233f0767ba656bde4c56d9
Thomas Facos <tjfacos@gmail.com>
2023-02-28 15:36:34 +0000

    The function can now tell if 2 components are connected

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,25 +1,43 @@
+function arrayEquals(a, b) {
+    console.log(a)
+    console.log(b)
+    
+    let val =  Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+    // console.log(val)
+    return val
+}
+
+// console.log(arrayEquals(
+//     [1,2,3],
+//     [1,2,3]
+// ))
+
 const CheckConnection = (comp1, comp2) => {
     // Return true if the 2 components have ports that overlap, false otherwise
     
 
     // Broken, check array comparison
-    console.log()
+    
 
+    // console.log(comp1.portCoords)
+    // console.log(comp2.portCoords)
 
-    console.log(comp1.portCoords)
-    console.log(comp2.portCoords)
+    let connected = false
 
     comp1.portCoords.forEach(portA => {
         comp2.portCoords.forEach(portB => {
-            console.log(JSON.stringify(portA))
-            console.log(JSON.stringify(portB))
             
-            if (JSON.stringify(portA) == JSON.stringify(portB)){
-                return true;
+            console.log(arrayEquals(portA, portB))
+            if (arrayEquals(portA, portB)){
+                console.log("MATCH")
+                connected = true;
             }
         })
     });
     
-    return false
+    return connected
 
 }
