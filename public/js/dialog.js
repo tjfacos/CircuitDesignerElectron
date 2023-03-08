dcd96717c576999686c7419f7241ffc150b7125b
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-03-08 21:11:57 +0000

    Added comment notes

--- public/js/dialog.js
+++ public/js/dialog.js
@@ -7,5 +7,14 @@ const closeHelpDialog = () => {
 }
 
 const ToggleEditor = () => {
+    // If the editor is not yet displayed, display it
+    // Set the name to the selected component
+    // Make sure emf is hidden unless component is a cell
+    // Add event Listeners to change the value of the resistance/emf on change
+    // Make sure all values are numbers
+
+    // If the editor is displayed, close it
+
     
+
 }
